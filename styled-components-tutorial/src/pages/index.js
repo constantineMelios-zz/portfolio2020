8c8a7419e4bddc48c207593ee9fccadc58d9381f
Konstantinos Melios <constantinemelios@gmail.com>
2020-11-05 11:06:43 +0200

    Reseting CSS

--- styled-components-tutorial/src/pages/index.js
+++ styled-components-tutorial/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import GlobalStyle from '../styles/GlobalStyle'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello Constantine</h1>
+    </>
+  )
 }
