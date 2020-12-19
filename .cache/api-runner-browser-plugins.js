b04a722e8815d38eb773014e7ba2b59b97f2d2a1
Constantine Melios <constantinemelios@gmail.com>
2020-12-19 15:57:07 +0200

    Deleted not needed files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-react-redux/gatsby-browser.js'),
-      options: {"plugins":[],"pathToCreateStoreModule":"./src/redux/createStore","serialize":{"space":0,"isJSON":true,"unsafe":false,"ignoreFunction":true}},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
